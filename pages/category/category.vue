a88799e54a6cf5b14219658146d30928f715067a
matsunonanamatsu <374513203@qq.com>
2023-05-30 11:39:22 +0800

    version1

---
+++ pages/category/category.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<text>hello im {{name}}</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				name:'category'
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/home/home.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<text>hello im home</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
